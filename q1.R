1fd6d37cd411de336107618d5aa7d603b1d71b82
Karla Sophia Cruz <karlasophiacruz2001@gmail.com>
2021-07-19 22:41:39 -0300

    final update q1

--- q1.R
+++ q1.R
@@ -1,6 +1,32 @@
 lista <- c(48, 58, 56, 63, 52, 50, 59, 51, 59, 38, 57, 56, 73, 61, 41, 55, 49, 61, 49, 49, 52, 55, 60, 52, 54,
            57, 47, 66, 60, 53, 59, 50, 45, 57, 64, 56, 57, 60, 47, 58, 53, 58, 66, 47, 40)
 
+#############################################################
+
+#LETRA A: HISTOGRAMA DA FREQUÊNCIA DA LISTA DE NÚMEROS AGRUPADOS
+
+lim_intervalo <- c(38, 43, 48, 53, 58, 63, 68, 73)
+
+lista_df <- as.data.frame(table(cut(lista, breaks = lim_intervalo)))
+
+grid(nx = NA, ny = NULL, lty = 2, col = "gray", lwd = 1)
+
+h_list = hist(lista, col = "khaki", add = TRUE,
+              main = "Histograma da Frequência da Lista de Números Agrupados",
+              xlab = "Lista de Números Agrupados", ylab = "Frequência Absoluta",
+              xlim = c(35, 75), ylim = c(0, 15),
+              xaxp = c(38, 73, 7),
+              labels = as.character(lista_df$Freq), breaks = lim_intervalo)
+
+xfit <- seq(min(lista), max(lista), length = 200)
+yfit <- dnorm(xfit, mean(lista), sd(lista))
+yfit <- yfit*diff(h_list$mids[1:2])*length(lista)
+lines(xfit, yfit, col = "midnightblue", lwd = 2)
+
+#############################################################
+
+#################### FUNÇÃO MODA CZUBER #####################
+
 moda.czuber <- function(dados){
   amp <- max(dados) - min(dados)
   nk <- round(1 + 3.222 * log10(length(dados)))
@@ -17,45 +43,52 @@ moda.czuber <- function(dados){
   return(lim_inferior + (d1 / (d1 + d2)) * amp_classe)
 }
 
-#LETRA A
+#############################################################
 
-lim_range <- c(38, 43, 48, 53, 58, 63, 68, 73)
+#LETRA B: MÉDIA, MODA DE CZUBER, MEDIANA, TERCEIRO QUARTIL, PERCENTIS 8, 50 E 80
 
-range <- table(cut(lista, breaks = lim_range))
-
-lista_df <- as.data.frame(range)
-
-h_list = hist(lista, col = c("pink", "cyan"),  
-              main = "Histograma da Lista",
-              xlab = "Lista", ylab = "Frequência",
-              xlim = c(35, 75), ylim = c(0, 15),
-              xaxp = c(38, 73, 7),
-              labels = as.character(lista_df$Freq), breaks = lim_range)
-
-lines(c(min(h_list$breaks), h_list$mids, max(h_list$breaks)), 
-      c(0,h_list$counts, 0), type = "l")
-
-help("lines")
-
-#LETRA B
-
+#MÉDIA
 media <- mean(lista)
 
+#MEDIANA
 mediana <- median(lista)
 
+#MODA DE CZUBER
 moda_czuber <- moda.czuber(lista)
 
+#TERCEIRO QUARTIL
 terceiro_quartil <- quantile(lista, 0.75)
 
+#PERCENTIL 8
 percentil_8 <- quantile(lista, 0.08)
 
+#PERCENTIL 50
 percentil_50 <- quantile(lista, 0.5)
 
+#PERCENTIL 80
 percentil_80 <- quantile(lista, 0.8)
 
-#LETRA C
+#############################################################
 
-boxplot(lista)
+#LETRA C: BOXPLOT
+
+#BOXPLOT VERTICAL
+
+grid(nx = NA, ny = NULL, lty = 2, col = "gray", lwd = 1)
+
+boxplot_lista_v <- boxplot(lista, add = TRUE,
+                         main = "Boxplot da Lista de Números",
+                         ylab = "Lista de Números",
+                         col = "khaki")
+
+#BOXPLOT HORIZONTAL
+
+boxplot_lista_h <- boxplot(lista, horizontal = TRUE, notch = TRUE,
+                         main = "Boxplot da Lista de Números",
+                         ylab = "Lista de Números",
+                         col = "khaki")
+
+#############################################################
 
 
 
