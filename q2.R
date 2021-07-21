7a64225b57e50d038b64c7ae4b14100a411de2d1
Karla Sophia Cruz <karlasophiacruz2001@gmail.com>
2021-07-21 00:28:54 -0300

    Atividade Finalizada

--- q2.R
+++ q2.R
@@ -14,10 +14,12 @@ grid <- grid(nx = NA, ny = NULL, lty = 2, col = "gray", lwd = 1)
 
 #LETRA A: GRÁFICO DE BARRAS DA SITUAÇÃO ATUAL DOS PACIENTES
 
-barplot_situacao = barplot(sort((table(dados$situacao_atual)), decreasing = TRUE), 
-                           cex.names = 0.59, srt = 45,
-                           legend = TRUE, args.legend = list(x = 10, y = 150000, cex = 0.8, text.font = 2, bg = "ghostwhite"),
-                           col = c("blanchedalmond", "bisque", "bisque1", "burlywood1", "burlywood2", "bisque2", "burlywood", "bisque3"),
+gb_situacao = barplot(sort((table(dados$situacao_atual)), decreasing = TRUE), 
+                           cex.names = 0.59, srt = 45, ylim = c(0, 200000, 10000),
+                           legend = TRUE, args.legend = list(x = 10, y = 150000, 
+                           cex = 0.8, text.font = 2, bg = "ghostwhite"),
+                           col = c("blanchedalmond", "bisque", "bisque1", "burlywood1", 
+                                   "burlywood2", "bisque2", "burlywood", "bisque3"),
                            main = "GRÁFICO DE BARRAS DA SITUAÇÃO ATUAL DOS PACIENTES")
 
 #############################################################
@@ -37,28 +39,30 @@ gb_obitos_muni <- barplot(sort(obitos_muni, decreasing = TRUE), col = "bisque2",
 
 #LETRA C: GRÁFICO DOS ÓBITOS POR SEXO E IDADE
 
-lim_idade <- c("0 - 10", "10 - 20", "20 - 30", "30 - 40", "40 - 50", "50 - 60", "60 - 70", "70 - 80", "80 - 90", "90 - 100")
+lim_idade <- c("0 - 10", "10 - 20", "20 - 30", "30 - 40", "40 - 50", "50 - 60", 
+               "60 - 70", "70 - 80", "80 - 90", "90 - 100")
 
 obitos_idade <- cut(obitos$idade, seq(0, 100, by = 10))
 
 obitos_sexo_idade <- table(obitos$sexo, obitos_idade)
 
-gb_obitos_sexidade <- barplot(obitos_sexo_idade, beside = TRUE,  col = c("mistyrose2", "lightskyblue1"),
-                  main = "GRÁFICO DOS ÓBITOS POR SEXO E IDADE", ylim = c(0, 700), add = TRUE,
-                  names.arg = lim_idade, legend = TRUE, args.legend = list(x = 10, y = 550, cex = 0.8, text.font = 2, bg = "ghostwhite"),
-                  cex.axis = 0.8, ylab = "Número de óbitos", xlab = "Sexo e Idade (em anos)")
-
+gb_obitos_sexidade <- barplot(obitos_sexo_idade, beside = TRUE,  
+                              col = c("mistyrose2", "lightskyblue1"),
+                              main = "GRÁFICO DOS ÓBITOS POR SEXO E IDADE", 
+                              ylim = c(0, 700), add = TRUE, cex.axis = 0.8,
+                              names.arg = lim_idade, legend = TRUE,
+                              args.legend = list(x = 10, y = 550, cex = 0.8, text.font = 2, 
+                                                 bg = "ghostwhite"),
+                              ylab = "Número de óbitos", xlab = "Sexo e Idade (em anos)")
 
 #############################################################
 
 #LETRA D: HISTOGRAMA DE ÓBITOS POR IDADE
 
-obidade_df <- as.data.frame(table(cut(obitos$idade, breaks =  seq(0, 110, by = 11))))
-
-h_obitos_idade <- hist(obitos$idade, col = "khaki", main = "HISTOGRAMA DE ÓBITOS POR IDADE", 
-                  labels = as.character(obidade_df$Freq),
-                  xlab = "Idade (em anos)", ylab = "Óbitos", add = TRUE,
-                  xlim = c(0, 110), ylim = c(0, 1300),  xaxp = c(0, 110, 11))
+h_obitos_idade <- hist(obitos$idade, col = "khaki", 
+                       main = "HISTOGRAMA DE ÓBITOS POR IDADE",
+                       xlab = "Idade (em anos)", ylab = "Óbitos", add= TRUE, 
+                       xlim = c(0, 110), ylim = c(0, 1300),  xaxp = c(0, 110, 11))
 
 xfit <- seq(min(obitos$idade), max(obitos$idade), length = 100)
 yfit <- dnorm(xfit, mean(obitos$idade), sd(obitos$idade))
@@ -75,7 +79,10 @@ lim_datas <- c("01 / 20", "02 / 20", "03 / 20", "04 / 20", "05 / 20", "06 / 20",
 
 obito_data <- as.Date(obitos$data_resultado_exame)
 obito_data <- table(format(obito_data, '%y %m'))
+
 gb_obitos_mes <- barplot(date, col = "khaki", add = TRUE,
                         main = "GRÁFICO DE BARRAS DOS ÓBITOS POR MÊS",
                         xlab = "Período (mês / ano)", ylab = "Óbitos",
                         ylim = c(0, 1000), names.arg = lim_datas)
+
+#############################################################
--- q3.R
+++ q3.R
@@ -1,31 +1,53 @@
-craibas.idade <- cut(obitos.craibas$idade, breaks)
+dados <- read.csv("dadosPacientes2021.csv")
+
+dados[dados == "Mascuino"] <- "Masculino"
+
+check_20 <- grepl(("2020"), dados$data_resultado_exame, fixed = TRUE)
+check_21 <- grepl(("2021"), dados$data_resultado_exame, fixed = TRUE)
+
+dados <- subset(dados, check_20 == TRUE | check_21 == TRUE)
+dados <- subset(dados, idade > 0 & idade < 120)
+
+grid <- grid(nx = NA, ny = NULL, lty = 2, col = "gray", lwd = 1)
+
+dados.craibas <- subset(dados, municipio_residencia == "Craíbas")
+
+#############################################################
+
+#GRÁFICO DOS ÓBITOS DE CRAÍBAS POR SEXO E IDADE
+
+obitos.craibas <- subset(dados.craibas, situacao_atual == "Óbito")
+obitos.craibas.idade <- cut(obitos.craibas$idade, seq(0, 100, by = 10))
+
 plot.obitos.sexo <- table(obitos.craibas$sexo, obitos.craibas.idade)
 
-barplot(plot.obitos.sexo, beside=TRUE,
-        col=c("mistyrose2", "lightskyblue1", ylim=c(0, 500), names.arg= names,
-        las=2,  main="Gráfico do número de obitos no estado de Craíbas")
-        xlab="Idade dividida em anos", ylab= "Número de Óbitos")
+lim_idade <- c("0 - 10", "10 - 20", "20 - 30", "30 - 40", "40 - 50", "50 - 60", 
+               "60 - 70", "70 - 80", "80 - 90", "90 - 100")
+
+gb_craibas <- barplot(plot.obitos.sexo, beside = TRUE, add = TRUE,
+        col = c("mistyrose2", "lightskyblue1"), names.arg = lim_idade,
+        main = "GRÁFICO DE ÓBITOS POR SEXO E IDADE EM CRAÍBAS", legend = TRUE,
+        args.legend = list(x = 10, y = 4, cex = 0.8, text.font = 2, 
+                           bg = "ghostwhite"),
+        xlab = "Idade (em anos)", ylab= "Número de Óbitos")
           
-legend(topright, pch=15, col=c("mistyrose2", "lightskyblue1"),
-       legend= c("Homens", "Mulheres"), cex=1.25,
-       bty="n")
-###################################################################        
-        
-        
-        
+############################################################# 
 
+#GRÁFICO DE CONTAMINAÇÕES EM CRAÍBAS POR SEXO E IDADE
 
+dados.craibas.idade <- table(dados.craibas$idade)
 
-craibas.idade <- cut(dados.pacientes.craibas$idade, breaks)
-plot..sexo <- table(dados.pacientes.craibas$sexo, craibas.idade)
+craibas_idade <- cut(dados.craibas$idade, seq(0, 100, by = 10))
 
-barplot(plot.contaminacao.sexo, beside=TRUE,
-        las=2, ylim=c(0, 12000)
-        col=c("mistyrose2", "lightskyblue1", names.arg= names,
-        xlab="Idade dividida em anos", ylab="Número de indivívuos contaminados",
-        main="Gráfico do número de contaminados no estado de Craíbas")
-        
-legend(topright, pch=15, col=c("mistyrose2", "lightskyblue1"),
-       legend= c("Homens", "Mulheres"), cex=1.25,
-       bty="n")
+casos.confirmados <- subset(dados.craibas, classificacao == "Confirmado")
+
+casos.craibas <- table(casos.confirmados$sexo, craibas_idade) 
+
+gb_craibas2 <- barplot(casos.craibas, beside = TRUE, add = TRUE,
+        col = c("mistyrose2", "lightskyblue1"), names.arg = lim_idade,
+        xlab = "Idade (em anos)", ylab = "Número de Contaminações",
+        main = "GRÁFICO DE CONTAMINAÇÕES POR SEXO E IDADE EM CRAÍBAS",
+        legend = TRUE, args.legend = list(x = 30, y = 130, cex = 0.8, text.font = 2, 
+                                           bg = "ghostwhite"))
+
 ######################################################################
